2a3d23d05569a3212c6f17b26b10f08fe3b5b29b
eugenioclrc <eugenioclrc@gmail.com>
2022-12-16 09:53:51 -0300

    fix localhost demo

--- server/index.js
+++ server/index.js
@@ -3,16 +3,27 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,
+ { cors: {
+    origin: "http://localhost:3000",
+    // or with an array of origins
+    // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
+    credentials: true
+  }
+});
 
+ 
+app.use(express.static('../client/dist'))
+
+/*
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-
+*/
 io.on("connection", (socket) => {
   console.log("a user connected");
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+server.listen(5000, () => {
+  console.log("listening on *:5000");
 });
