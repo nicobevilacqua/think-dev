7c0ec2dcdf01866944103f1f169c31f6b86dc19b
eugenioclrc <eugenioclrc@gmail.com>
2022-12-16 11:40:16 -0300

    basic lands test

--- nft-lands/test/Lands.t.sol
+++ nft-lands/test/Lands.t.sol
@@ -7,14 +7,27 @@ import "../src/Lands.sol";
 contract BoxLandsTest is Test {
     BoxLands public lands;
 
+    address user = makeAddr("user");
+
     function setUp() public {
         lands = new BoxLands();
     }
 
     function testSimpleMint() public {
+        vm.startPrank(user);
+
         assertEq(lands.totalSupply(), 0, "Land total supply should be 0");
         
-    }
+        lands.mint(0, 0, 0);
+        assertEq(lands.totalSupply(), 1, "Land total supply should be 1");
 
-    
+        (int256 expectedX, int256 expectedY, int256 expectedZ, bytes32 expectedHash) = lands.lands(1);
+
+        (int256 x, int256 y, int256 z, bytes32 hash) = lands.coordsToLandPos(12, 10, 10);
+        assertEq(x, expectedX, "x should be 0");
+        assertEq(y, expectedY, "y should be -30");
+        assertEq(z, expectedZ, "z should be 0");
+        assertEq(hash, expectedHash, "Wrong hash");        
+    }
+        
 }
